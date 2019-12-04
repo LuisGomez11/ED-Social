a60b8cb7ae1bb4b1b0f9b459ec3aa6ff54ed2ed1
LuisGomez11 <luisgomez24g@gmail.com>
2019-12-03 20:20:24 -0500

    Login

--- Backend/src/Controllers/User.controller.js
+++ Backend/src/Controllers/User.controller.js
@@ -15,15 +15,53 @@ UserCtrl.getUsers = async (req, res) => {
 UserCtrl.postUser = async (req, res) => {
     req.body.password = CryptoJS.AES.encrypt(req.body.password, Config.Encrypt);
     await new User(req.body).save().then(user => {
-        const User = user.toJSON();
-        delete User.password;
-        return res.status(user !== null ? 200 : 404).send({
-            ...User,
-            Token: CreateToken(user.toJSON())
-        });
+        const token = CreateToken(user.toJSON());
+        res.status(user !== null ? 200 : 404).send({token});
     }).catch(err => {
         return res.status(406).send(err);
     });
 }
+
+// Login usuario
+UserCtrl.loginUser = async (req, res) => {
+    const { userName, password } = req.body;
+    const user = await User.findOne({userName});
+    if(!user) return res.status(401).send("Email doen't exists");
+    const decryptedPassword = CryptoJS.AES.decrypt(user.toJSON().password, Config.Encrypt).toString(CryptoJS.enc.Utf8);
+    // console.log(decryptedPassword);
+    if(decryptedPassword !== password) return res.status(401).send("Incorrect password");
+    const UserModel = user.toJSON();
+    delete UserModel.password;
+    const token = CreateToken(UserModel);
+    return res.status(200).send({
+        User: UserModel,
+        Token: token
+    });
+}
+
+UserCtrl.verifyToken = async(req, res, next) => {
+    try {
+        if (!req.headers.authorization) {
+            return res.status(401).send('Unauhtorized Request');
+        }
+
+        let token = req.headers.authorization.split(' ')[1];
+        if (token === 'null') {
+            return res.status(401).send('Unauhtorized Request');
+        }
+
+        const payload = await jwt.verify(token, Config.Token);
+
+        if (!payload) {
+            return res.status(401).send('Unauhtorized Request');
+        }
+
+        req.userId = payload._id;
+        next();
+    } catch (e) {
+        return res.status(401).send('Unauhtorized Request');
+    }
+}
+
 
 module.exports = UserCtrl;
--- Backend/src/Routes/UserRoutes.js
+++ Backend/src/Routes/UserRoutes.js
@@ -2,8 +2,10 @@ const UserCtrl = require('../Controllers/User.controller');
 const express = require('express');
 const router = express.Router();
 
-router.get('/user', UserCtrl.getUsers);
+router.get('/user', UserCtrl.verifyToken, UserCtrl.getUsers);
 
-router.post('/user/create', UserCtrl.postUser); //Registrar usuario
+router.post('/user/signup', UserCtrl.postUser); //Registrar usuario
+
+router.post('/user/login', UserCtrl.loginUser); //Login usuario
 
 module.exports = router;
