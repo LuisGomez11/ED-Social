252637a90b403e4e1ee9b874d6671bf6d8ced04c
LuisGomez11 <luisgomez24g@gmail.com>
2019-12-06 01:05:07 -0500

    Changes

---
+++ Backend/Configs/app.config.js
@@ -0,0 +1,6 @@
+exports.Config = {
+    port: process.env.PORT || 3000,
+    db: process.env.MONGO || 'mongodb://luisgomez11:luis123456@ds211875.mlab.com:11875/edsocial',
+    Token: process.env.Token ||'SecretToken',
+    Encrypt: process.env.Encrypt || 'FHFWIUFHEFUEFHNJDSFNDsknskfnwiwikwfwf.fror2-rp29o2292--<'
+}
--- Backend/src/Configs/app.config.js
+++
@@ -1,6 +0,0 @@
-exports.Config = {
-    port: process.env.PORT || 3000,
-    db: process.env.MONGO || 'mongodb://localhost/edsocial',
-    Token: process.env.Token ||'SecretToken',
-    Encrypt: process.env.Encrypt || 'FHFWIUFHEFUEFHNJDSFNDsknskfnwiwikwfwf.fror2-rp29o2292--<'
-}
