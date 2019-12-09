3ffe6011ae1e7248a2fe532df874f64be2eba722
Luis Alberto Gomez Arteaga <luisgomez24g@gmail.com>
2019-12-09 01:55:23 -0500

    change

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -5,9 +5,9 @@ const api = '/api'
 export const environment = {
   production: true,
   Encrypt: 'sfkhehfuewfhefheoIFHIFEHFIOEWGHEWIGOHasfe<g<e<24<22<24242-.4-.2{+´242222osrjwi',
-  urlUsers: local + api + '/user',
-  urlSignUp: local + api + '/user/signup',
-  urlLogin: local + api + '/user/login',
-  urlPublication: local + api + '/publication',
-  urlConversation: local + api + '/conversation/'
+  urlUsers: api + '/user',
+  urlSignUp: api + '/user/signup',
+  urlLogin: api + '/user/login',
+  urlPublication: api + '/publication',
+  urlConversation: api + '/conversation/'
 };
